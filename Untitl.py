386b624d8b062bbf32756d84a9b36fbc781f421b
AitanKGTU <155081817+AitanKGTU@users.noreply.github.com>
2023-12-28 21:42:27 +0600

    Ex2
    Задания 1,2  с лаб2

---
+++ Untitl.py
@@ -0,0 +1,45 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "source": [
+    "input_string = input(\"Введите строку: \") \n",
+    "\n",
+    "words = input_string.split(';') \n",
+    "\n",
+    "longest_word = max(words, key=len) \n",
+    "\n",
+    "print(\"Самое длинное слово:\", longest_word) \n",
+    ""
+   ],
+   "outputs": [
+    {
+     "output_type": "stream",
+     "name": "stdout",
+     "text": [
+      "Самое длинное слово: Прекрасный\n"
+     ]
+    }
+   ],
+   "metadata": {}
+  }
+ ],
+ "nbformat": 4,
+ "nbformat_minor": 2,
+ "metadata": {
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": 3
+  },
+  "orig_nbformat": 4
+ }
+}
